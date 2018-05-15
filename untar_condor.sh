9ac982fb5f91ad6d35ebb0c422d9204386d40ec5
Elena Reinisch <ebaluyut@wisc.edu>
2018-05-15 10:14:45 -0500

    untar_condor.sh: initialize sym links from preproc dir (ECR 20180515)

--- untar_condor.sh
+++ untar_condor.sh
@@ -5,15 +5,16 @@
 # edit ECR 20180404 correct metadata files to make soft links in preproc dir; check that links don't exist before trying to make them
 # edit ECR 20180418 fix pathnames to LED and PRM files
 # edit ECR 20180511 fix symlink for slav LED and PRM files
+# edit ECR 20180515 fix symlink for LED and PRM files to establish link from within preproc dir
 
 # get list of pair tar files
-ls In*.tgz > tarlist
+ls -d In* > tarlist #.tgz > tarlist
 cpwd=`pwd`
 
 while read -r a; do 
   echo $a
   # untar interferogram directory
-  tar -xzvf $a
+ # tar -xzvf $a
   # get pair and mast/slav names
   pair=`echo $a | awk -F. '{print $1}'`
   mast=`echo $pair | awk -F_ '{print $1}' | awk -Fn '{print $2}'`
@@ -24,37 +25,47 @@ while read -r a; do
   mastPRM=`find $pair -name *${mast}*PRM`
   slavPRM=`find $pair -name *${slav}*PRM`
   # copy PRM and LED files from preproc directory if they aren't in pair directory already
+  preproc_dir=`pwd | awk -F/ '{print $1"/"$2"/"$3"/"$4"/"$5"/preproc"}'`
   if [[ -z $mastLED ]]
   then
-     preproc_dir=`pwd | awk -F/ '{print $1"/"$2"/"$3"/"$4"/"$5"/preproc/"}'`
-     cp $preproc_dir/$mast.LED $pair/
-     cp $preproc_dir/$mast.PRM $pair/
-     cp $preproc_dir/$slav.LED $pair/
-     cp $preproc_dir/$slav.PRM $pair/
+     cp $preproc_dir/*$mast*.LED $pair/
      mastLED=`find $pair -name *${mast}*LED`
-     slavLED=`find $pair -name *${slav}*LED`
+  fi
+  if [[ -z $mastPRM ]]
+  then
+     cp $preproc_dir/*$mast*.PRM $pair/
      mastPRM=`find $pair -name *${mast}*PRM`
+  fi
+  if [[ -z $slavLED ]]
+  then
+     cp $preproc_dir/*$slav*.LED $pair/
+     slavLED=`find $pair -name *${slav}*LED`
+  fi
+  if [[ -z $slavPRM ]]
+  then
+     cp $preproc_dir/*$slav*.PRM $pair/
      slavPRM=`find $pair -name *${slav}*PRM`
   fi
+
   # make links to preproc files
   if [[ ! -e ../preproc/${mast}.LED && ! -L ../preproc/$mast.LED ]]; then
     mastLEDname=`echo $mastLED | awk -F/ '{print $2}'`
     mastPRMname=`echo $mastPRM | awk -F/ '{print $2}'`
-    cd $pair
-    ln -s $mastLEDname ../../preproc/$mast.LED
-    ln -s $mastPRMname ../../preproc/$mast.PRM
-    cd ..
+    cd ../preproc
+    ln -s ../intf/$mastLED $mast.LED
+    ln -s ../intf/$mastPRM $mast.PRM
+    cd ../intf
   fi
   if [[ ! -e ../preproc/${slav}.LED && ! -L ../preproc/$slav.LED ]]; then 
     slavLEDname=`echo $slavLED | awk -F/ '{print $2}'`
     slavPRMname=`echo $slavPRM | awk -F/ '{print $2}'`
-    cd $pair
-    ln -s $slavLEDname ../../preproc/$slav.LED
-    ln -s $slavPRMname ../../preproc/$slav.PRM
-    cd ..
+    cd ../preproc
+    ln -s ../intf/$slavLED $slav.LED
+    ln -s ../intf/$slavPRM $slav.PRM
+    cd ../intf
   fi
 done < tarlist
-
+exit
 # clean up
 rm tarlist
 rm In*.tgz
